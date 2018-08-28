d38016de5cd783e23d8de73b0db8cd74ea2e4833
matarilo <kentaro.inomata@gmail.com>
2018-08-28 12:06:21 -0700

    Add XML comments

--- LanguageServer/Parameters/TextDocument/CodeActionParams.cs
+++ LanguageServer/Parameters/TextDocument/CodeActionParams.cs
@@ -4,12 +4,27 @@ using System.Text;
 
 namespace LanguageServer.Parameters.TextDocument
 {
+    /// <summary>
+    /// For <c>textDocument/codeAction</c>
+    /// </summary>
+    /// <remarks>
+    /// Params for the CodeActionRequest
+    /// </remarks>
     public class CodeActionParams
     {
+        /// <summary>
+        /// The document in which the command was invoked.
+        /// </summary>
         public TextDocumentIdentifier textDocument { get; set; }
 
+        /// <summary>
+        /// The range for which the command was invoked.
+        /// </summary>
         public Range range { get; set; }
 
+        /// <summary>
+        /// Context carrying additional information.
+        /// </summary>
         public CodeActionContext context { get; set; }
     }
 }
