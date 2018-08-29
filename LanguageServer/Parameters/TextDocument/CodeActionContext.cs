b9ef1260a87a78670fba0153b7a98cd24e903132
matarilo <kentaro.inomata@gmail.com>
2018-08-29 11:10:22 -0700

    Support Spec v3.7.0.

--- LanguageServer/Parameters/TextDocument/CodeActionContext.cs
+++ LanguageServer/Parameters/TextDocument/CodeActionContext.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LanguageServer.Parameters.TextDocument
+﻿namespace LanguageServer.Parameters.TextDocument
 {
+    /// <summary>
+    /// For <c>textDocument/codeAction</c>
+    /// </summary>
+    /// <remarks>
+    /// Contains additional diagnostic information about the context in which a code action is run.
+    /// </remarks>
     public class CodeActionContext
     {
+        /// <summary>
+        /// An array of diagnostics.
+        /// </summary>
         public Diagnostic[] diagnostics { get; set; }
     }
 }
--- LanguageServer/Parameters/TextDocument/Diagnostic.cs
+++ LanguageServer/Parameters/TextDocument/Diagnostic.cs
@@ -2,16 +2,49 @@
 
 namespace LanguageServer.Parameters.TextDocument
 {
-    public class Diagnostic
-    {
-        public Range range { get; set; }
+	/// <summary>
+	/// For <c>textDocument/codeAction</c> and <c>textDocument/publishDiagnostics</c>
+	/// </summary>
+	/// <seealso>Spec 3.7.0</seealso>
+	public class Diagnostic
+	{
+		/// <summary>
+		/// The range at which the message applies.
+		/// </summary>
+		public Range range { get; set; }
 
-	    public DiagnosticSeverity? severity { get; set; }
+		/// <summary>
+		/// The diagnostic's severity. Can be omitted.
+		/// </summary>
+		/// <remarks>
+		/// If omitted it is up to the client to interpret diagnostics as error, warning, info or hint.
+		/// </remarks>
+		public DiagnosticSeverity? severity { get; set; }
 
-	    public NumberOrString code { get; set; }
+		/// <summary>
+		/// The diagnostic's code. Can be omitted.
+		/// </summary>
+		public NumberOrString code { get; set; }
 
-        public string source { get; set; }
+		/// <summary>
+		/// A human-readable string describing the source of this diagnostic,
+		/// e.g. <c>'typescript'</c> or <c>'super lint'</c>.
+		/// </summary>
+		public string source { get; set; }
 
-    	public string message { get; set; }
-    }
+		/// <summary>
+		/// The diagnostic's message.
+		/// </summary>
+		public string message { get; set; }
+
+		/// <summary>
+		/// An array of related diagnostic information.
+		/// </summary>
+		/// <remarks>
+		/// e.g. when symbol-names within a scope collide
+		/// all definitions can be marked via this property.
+		/// </remarks>
+		/// <seealso>Spec 3.7.0</seealso>
+		public DiagnosticRelatedInformation[] relatedInformation { get; set; }
+	}
 }
